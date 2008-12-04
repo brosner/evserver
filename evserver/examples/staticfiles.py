2d8912d8868a7de5f4be514ec9914fa5952a6ba3
majek04 <majek04@e9bb6d7e-af12-11dd-bad7-87afd3b02348>
2008-12-04 19:38:47 +0000

    more spawning benchmarks
    git-svn-id: http://evserver.googlecode.com/svn/trunk@43 e9bb6d7e-af12-11dd-bad7-87afd3b02348

--- evserver/examples/staticfiles.py
+++ evserver/examples/staticfiles.py
@@ -10,30 +10,64 @@ file_cache = {}
 
 def application(environ, start_response):
     #form = dict(cgi.parse_qsl(environ.get('QUERY_STRING', '')))
-    path = os.path.normpath(os.path.join(ROOT, environ['PATH_INFO'][1:]))
+    path = environ['PATH_INFO']
+    path = os.path.normpath(path)
 
     if path not in file_cache:
         if not path.startswith(ROOT):
-            start_response("403 Forbidden", [('Content-type','text/plain')])
-            return ('403 Access Denied',)
+            file_cache[path] = ("403 Forbidden", [('Content-type','text/plain')], '403 Access Denied')
+        elif not os.path.isfile(path):
+            file_cache[path] = ('404 Not Found', [('Content-type','text/plain')], '404 Not Found')
+        elif not os.access(path, os.R_OK):
+            file_cache[path] = ("403 Forbidden", [('Content-type','text/plain')], '403 Access Denied')
+        else:
+            fd = open(path, 'rb')
+            data = fd.read() # could be changed to yielding if the file is slow
+            fd.close()
+    
+            mt  = mimetypes.guess_type(path)[0]
+            if not mt: mt = 'text/html; charset=utf-8'
+            headers = [('Content-type', mt)]
+            file_cache[path] = ("200 OK", headers, data)
 
-        if not os.path.isfile(path):
-            start_response("404 Not Found", [('Content-type','text/plain')])
-            return ('404 Not Found',)
-
-        if not os.access(path, os.R_OK):
-            start_response("403 Forbidden", [('Content-type','text/plain')])
-            return ('403 Access Denied',)
-
-        fd = open(path, 'rb')
-        data = fd.read() # could be changed to yielding if the file is slow
-        fd.close()
-
-        mt  = mimetypes.guess_type(path)[0]
-        if not mt: mt = 'text/html; charset=utf-8'
-        headers = [('Content-type', mt)]
-        file_cache[path] = (headers, data)
-
-    headers, data = file_cache[path]
-    start_response("200 OK", headers)
+    code, headers, data = file_cache[path]
+    start_response(code, headers)
     return [data]
+
+
+import socket
+def application_comet_spawn(environ, start_response):
+    start_response('200 OK', [('Content-type','text/plain')])
+    def iterator():
+        sd = socket.socket()
+        sd.bind( ('127.0.0.1', 0) )
+        sd.listen(2)
+        try:
+            for i in range(10):
+                sd.settimeout(0.1)
+                try:
+                    sd.accept()
+                except Exception: pass
+                yield 'ping\r\n' # this is not working for spawning
+        except GeneratorExit:
+            pass
+        sd.close()
+    return iterator()
+
+def application_comet_awsgi(environ, start_response):
+    start_response('200 OK', [('Content-type','text/plain')])
+    def iterator():
+        sd = socket.socket()
+        sd.bind( ('127.0.0.1', 0) )
+        sd.listen(2)
+        try:
+            for i in range(10):
+                yield environ['x-wsgiorg.fdevent.readable'](sd, 0.1)
+                yield 'ping\r\n'
+        except GeneratorExit:
+            pass
+        sd.close()
+    return iterator()
+
+
+
